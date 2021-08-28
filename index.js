b74603bdef137952de4a4bc021f1e95910d171c3
annie <annie@blackmagic.blackmagic>
2021-08-29 02:41:41 +0800

    feat: eslint, reurl generation, and valid url checking

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express');
+const https = require('https');
+const uniqueId = require('uniqid');
+const Console = require('console');
 
 const app = express();
 const PORT = 3000;
-const HOST = 'localhost';
+const HOST = 'http://localhost';
+const reurlQuery = '?reurl=';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/' , (req, res) => {
-    res.end('haha');
+function checkUrlValid(url) {
+  const req = https.request(url, (res) => {
+    Console.log(`statusCode: ${res.statusCode}`);
+  });
+
+  req.on('error', (error) => {
+    Console.error(error);
+  });
+
+  req.end();
+}
+
+app.get('/', (req, res) => {
+  const originUrl = req.query.url;
+  const newUrl = HOST + PORT + reurlQuery + uniqueId();
+  checkUrlValid(originUrl);
+  Console.log(originUrl);
+  res.end(newUrl);
 });
 
 app.listen(PORT, () => {
-    console.log(`Running on ${PORT}`);
+  Console.log('Running on %d', PORT);
 });
