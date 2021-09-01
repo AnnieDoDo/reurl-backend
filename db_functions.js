24c6ebf9986748e144d84987578100d90b7e87ca
annie <annie@blackmagic.blackmagic>
2021-09-01 11:06:58 +0800

    feat: ORM table with sequelize and add createLink

---
+++ db_functions.js
@@ -0,0 +1,104 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const uuid = require('uuid');
+const Console = require('console');
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './sqldev.db',
+});
+
+sequelize.authenticate()
+  .then(() => {
+    Console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    Console.error('Unable to connect to the database:', err);
+  });
+
+const User = sequelize.define('USER', {
+  ID: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true,
+  },
+  USERNAME: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  PASSWORD: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  freezeTableName: true,
+  timestamps: false,
+});
+Console.log(User === sequelize.models.User);
+
+const Link = sequelize.define('LINK', {
+  ID: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true,
+  },
+  URL: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  KEY: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  COUNT: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+}, {
+  freezeTableName: true,
+  timestamps: false,
+});
+
+Console.log(Link === sequelize.models.Link);
+
+module.exports = {
+  async createUser(username, password) {
+    try {
+      await User.upsert({
+        ID: uuid.v4(),
+        USERNAME: username,
+        PASSWORD: password,
+      });
+      return true;
+    } catch (e) {
+      return false;
+    }
+  },
+  async createLink(url, key) {
+    try {
+      await Link.upsert({
+        ID: uuid.v4(),
+        URL: url,
+        KEY: key,
+        COUNT: 0,
+      });
+      return true;
+    } catch (e) {
+      return false;
+    }
+  },
+  async SearchLink(key) {
+    try {
+      await Link.findOne({
+        attributes: ['KEY'],
+        where: {
+          Email: key,
+        },
+        raw: true,
+      });
+      return true;
+    } catch (e) {
+      return false;
+    }
+  },
+
+};
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const https = require('https');
 const uniqueId = require('uniqid');
 const Console = require('console');
+// const { path } = require('path');
+const sql = require('./db_functions');
 
 const app = express();
 const PORT = 3000;
@@ -11,23 +13,47 @@ const reurlQuery = '?reurl=';
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-function checkUrlValid(url) {
-  const req = https.request(url, (res) => {
-    Console.log(`statusCode: ${res.statusCode}`);
-  });
+const checkUrlValid = function UrlValid(url) {
+  return new Promise((resolve, reject) => {
+    const req = https.request(url, (res) => {
+      if (res.statusCode === 200) {
+        resolve({ isValid: true });
+      }
+      resolve({ isValid: false });
+    });
 
-  req.on('error', (error) => {
-    Console.error(error);
+    req.on('error', (error) => {
+      Console.error(error);
+      reject(error);
+    });
+    req.end();
   });
-
-  req.end();
-}
+};
 
 app.get('/', (req, res) => {
   const originUrl = req.query.url;
-  const newUrl = HOST + PORT + reurlQuery + uniqueId();
-  checkUrlValid(originUrl);
-  Console.log(originUrl);
+  const key = uniqueId();
+  const newUrl = HOST + PORT + reurlQuery + key;
+
+  async function isUrlValid() {
+    try {
+      const UrlValid = await checkUrlValid(originUrl);
+      if (UrlValid.isValid) {
+        return true;
+      }
+      res.end('Invalid URL');
+    } catch (err) {
+      Console.log('catch', err);
+      res.end('Invalid URL');
+    }
+    return false;
+  }
+  isUrlValid();
+
+  if (!sql.createLink(originUrl, key)) {
+    res.end('Fail to create link');
+  }
+
   res.end(newUrl);
 });
 
